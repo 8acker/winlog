80be8a9f4666112c07b3f3ae862de1e1e53c8eb9
Thami Bouchnafa <thami.bouchnafa@1und1.de>
2016-02-22 12:32:27 +0100

    added default options, lodash

--- index.js
+++ index.js
@@ -2,44 +2,47 @@
 
 var winston = require('winston');
 var _wrapper = require('./lib/cluster.wrapper');
+var _ = require('lodash');
+
+var default_winston_options = {
+    colors: {
+        debug: 'magenta',
+        info: 'green',
+        warning: 'yellow',
+        error: 'red'
+    },
+    transports: [
+        new winston.transports.Console(
+            {
+                level: 'debug',
+                json: false,
+                colorize: true,
+                prettyPrint: true,
+                handleExceptions: true
+            }
+        )
+    ],
+    exceptionHandlers: [
+        new winston.transports.File(
+            {
+                filename: 'logs/winston_exception_handlers.log',
+                json: false, maxsize: 1024 * 1024 * 10
+            }
+        )
+    ]
+};
+
+var default_daily_rotate_options = {
+    filename: 'logs/winston_',
+    datePattern: 'yyyy_MM_dd.log',
+    json: false,
+    maxsize: 1024 * 1024 * 10
+};
 
 module.exports = function(winston_options, daily_rotate_options) {
-    var _logger = new (winston.Logger)( winston_options ? winston_options :
-        {
-            colors: {
-                debug: 'magenta',
-                info: 'green',
-                warning: 'yellow',
-                error: 'red'
-            },
-            transports: [
-                new winston.transports.Console(
-                    {
-                        level: 'debug',
-                        json: false,
-                        colorize: true,
-                        prettyPrint: true,
-                        handleExceptions: true
-                    }
-                )
-            ],
-            exceptionHandlers: [
-                new winston.transports.File(
-                    {
-                        filename: 'logs/winston_exception_handlers.log',
-                        json: false, maxsize: 1024 * 1024 * 10
-                    }
-                )
-            ]
-        }
-    );
+    var _logger = new (winston.Logger)(_.assign(default_winston_options, winston_options));
 
-    _logger.add(require('winston-daily-rotate-file'), daily_rotate_options ? daily_rotate_options : {
-        filename: 'logs/winston_',
-        datePattern: 'yyyy_MM_dd.log',
-        json: false,
-        maxsize: 1024 * 1024 * 10
-    });
+    _logger.add(require('winston-daily-rotate-file'), _.assign(default_daily_rotate_options, daily_rotate_options));
 
     _logger._debug = function (msg) {
         _logger.debug(_wrapper(msg));
